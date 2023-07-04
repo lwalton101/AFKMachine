4ec791f9928922330bd3c772765589751f0e4252
lwalton101 <waltonfamily01@outlook.com>
2023-07-04 14:12:30 +0100

    basic config + bot

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const mineflayer = require("mineflayer")
+
+if(!fs.existsSync("config.json")){
+    let data = JSON.stringify({
+        "email": "nope",
+        "ip": "17.18.19.20"
+    })
+    fs.writeFileSync("config.json", data)
+}
+
+let rawdata = fs.readFileSync('config.json');
+let config = JSON.parse(rawdata);
+let email = config["email"]
+let ip = config["ip"]
+
+const bot = mineflayer.createBot({
+    host: ip,
+    username: email,
+    auth: "microsoft"
+})
